b6e33fc4ffce888e46fb4ac146c6c71816bfd106
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2025-02-06 23:39:10 +0200

    booleans challenge task
    - implement negation, conjunction, disjunction, implication, equivalence and respective operators
    - implement fold, conjunctionOfElements and disjunctionOfElements

--- src/main/scala/kse/unit2/challenge/booleans.scala
+++ src/main/scala/kse/unit2/challenge/booleans.scala
@@ -7,41 +7,72 @@ object booleans:
   case object True
   case object False
 
-  type True  = True.type
-  type False = False.type
-  type Boolean
+  type True    = True.type
+  type False   = False.type
+  type Boolean = True | False
 
-  val negation: Boolean => Boolean = ???
+  val negation: Boolean => Boolean =
+    case True  => False
+    case False => True
 
-  val conjunction: (Boolean, => Boolean) => Boolean = ???
+  val conjunction: (Boolean, => Boolean) => Boolean =
+    (left, right) =>
+      if left == False then False
+      else
+        (left, right) match
+          case (True, value)  => value
+          case (False, value) => False
 
-  val disjunction: (Boolean, => Boolean) => Boolean = ???
+  val disjunction: (Boolean, => Boolean) => Boolean =
+    (left, right) =>
+      if left == True then True
+      else
+        (left, right) match
+          case (True, value)  => True
+          case (False, value) => value
 
-  val implication: (Boolean, => Boolean) => Boolean = ???
+  val implication: (Boolean, => Boolean) => Boolean =
+    (left, right) =>
+      if left == False then True
+      else
+        (left, right) match
+          case (True, value)  => value
+          case (False, value) => True
 
-  val equivalence: (Boolean, => Boolean) => Boolean = ???
+  val equivalence: (Boolean, => Boolean) => Boolean =
+    (left, right) =>
+      left match
+        case True  => right
+        case False => negation(right)
 
   extension (value: Boolean)
 
     @targetName("Negation")
-    infix def unary_! : Boolean = ???
+    infix def unary_! : Boolean = negation(value)
 
     @targetName("Conjunction")
-    infix def ∧(that: => Boolean): Boolean = ???
+    infix def ∧(that: => Boolean): Boolean = conjunction(value, that)
 
     @targetName("Disjunction")
-    infix def ∨(that: => Boolean): Boolean = ???
+    infix def ∨(that: => Boolean): Boolean = disjunction(value, that)
 
     @targetName("Implication")
-    infix def →(that: => Boolean): Boolean = ???
+    infix def →(that: => Boolean): Boolean = implication(value, that)
 
     @targetName("Equivalence")
-    infix def ↔(that: => Boolean): Boolean = ???
+    infix def ↔(that: => Boolean): Boolean = equivalence(value, that)
 
-  def fold(operation: (Boolean, Boolean) => Boolean, unit: Boolean)(list: List[Boolean]): Boolean = ???
+  def fold(operation: (Boolean, Boolean) => Boolean, unit: Boolean)(list: List[Boolean]): Boolean =
+    @tailrec
+    def foldReq(list: List[Boolean], acc: Boolean): Boolean =
+      list match
+        case Nil          => acc
+        case head :: tail => foldReq(tail, operation(head, acc))
 
-  val conjunctionOfElements: List[Boolean] => Boolean = ???
-  val disjunctionOfElements: List[Boolean] => Boolean = ???
+    foldReq(list, unit)
+
+  val conjunctionOfElements: List[Boolean] => Boolean = fold((left, right) => left ∧ right, True)
+  val disjunctionOfElements: List[Boolean] => Boolean = fold((left, right) => left ∨ right, False)
 
   extension (booleans: List[Boolean])
     infix def conjunction: Boolean = conjunctionOfElements(booleans)

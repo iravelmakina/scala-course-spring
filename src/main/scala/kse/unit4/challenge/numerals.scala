21667a014d9bd40f0c8c516a5febce6cb5d5549f
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2025-03-14 19:43:22 +0200

    Numerals (#5)
    * implement Numeral, Zero and Successor
    
    * tests
    
    - add lazy generator for Successor, provide arbitrary instances for Zero, Successor and Numeral
    - add isZeroSpecification, SuccessorSpecification, PredecessorSpecification, AdditionSpecification, SubtractionSpecification, ComparisonSpecification tests
    
    * Zero and Successor updates
    
    - fix toString methods
    - override hashCode and change equals methods
    
    * test updates
    
    - remove preconditions from tests where they can be replaced with generation of random ordered values
    - add CombinedOperationsSpecification with test of + and -
    
    * hashCode methods fix
    
    * change tests to avoid using val

--- src/main/scala/kse/unit4/challenge/numerals.scala
+++ src/main/scala/kse/unit4/challenge/numerals.scala
@@ -10,19 +10,19 @@ object numerals:
 
     def predecessor: Numeral
 
-    def successor: Numeral = ???
+    def successor: Numeral = Successor(this)
 
     @targetName("greater than")
     infix def >(that: Numeral): Boolean
 
     @targetName("greater or equal to")
-    infix def >=(that: Numeral): Boolean = ???
+    infix def >=(that: Numeral): Boolean = !(that > this)
 
     @targetName("less than")
-    infix def <(that: Numeral): Boolean = ???
+    infix def <(that: Numeral): Boolean = that > this
 
     @targetName("less or equal to")
-    infix def <=(that: Numeral): Boolean = ???
+    infix def <=(that: Numeral): Boolean = !(this > that)
 
     @targetName("addition")
     infix def +(that: Numeral): Numeral
@@ -39,47 +39,60 @@ object numerals:
 
   object Zero extends Numeral:
 
-    def isZero: Boolean = ???
+    def isZero: Boolean = true
 
-    def predecessor: Numeral = ???
+    def predecessor: Numeral = this
 
     @targetName("greater than")
-    infix def >(that: Numeral): Boolean = ???
+    infix def >(that: Numeral): Boolean = false
 
     @targetName("addition")
-    infix def +(that: Numeral): Numeral = ???
+    infix def +(that: Numeral): Numeral = that
 
     // Optional
     @targetName("subtraction")
-    infix def -(that: Numeral): Numeral = ???
+    infix def -(that: Numeral): Numeral = this
 
-    def toInt: Int = ???
+    def toInt: Int = 0
 
-    override def toString: String = ???
+    override def toString: String = toInt.toString
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(that: Any): Boolean = that.isInstanceOf[Zero]
+
+    override def hashCode: Int = 0
 
   object Successor:
     def unapply(successor: Successor): Option[Numeral] = Option(successor.predecessor)
 
   class Successor(n: Numeral) extends Numeral:
 
-    def isZero: Boolean = ???
+    def isZero: Boolean = false
 
-    def predecessor: Numeral = ???
+    def predecessor: Numeral = n
 
     @targetName("greater than")
-    infix def >(that: Numeral): Boolean = ???
+    infix def >(that: Numeral): Boolean =
+      that match
+        case Zero         => true
+        case Successor(m) => predecessor > m
 
     @targetName("addition")
-    infix def +(that: Numeral): Numeral = ???
+    infix def +(that: Numeral): Numeral = Successor(predecessor + that)
 
     // Optional
     @targetName("subtraction")
-    infix def -(that: Numeral): Numeral = ???
+    infix def -(that: Numeral): Numeral =
+      that match
+        case Zero         => this
+        case Successor(m) => predecessor - m
 
     def toInt: Int = 1 + n.toInt
 
-    override def toString: String = ???
+    override def toString: String = toInt.toString
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(that: Any): Boolean =
+      that match
+        case that: Successor => that.predecessor == this.predecessor
+        case _               => false
+
+    override def hashCode: Int = 1 + predecessor.hashCode
--- src/test/scala/kse/unit4/challenge/NumeralsSpecification.scala
+++ src/test/scala/kse/unit4/challenge/NumeralsSpecification.scala
@@ -11,4 +11,168 @@ object NumeralsSpecification extends Properties("Numerals"):
   override def overrideParameters(p: Parameters): Parameters =
     p.withMinSuccessfulTests(50).withMaxDiscardRatio(100)
 
+  include(isZeroSpecification)
+  include(SuccessorSpecification)
+  include(PredecessorSpecification)
+  include(AdditionSpecification)
+  include(SubtractionSpecification)
+  include(CombinedOperationsSpecification)
+  include(ComparisonSpecification)
+
 end NumeralsSpecification
+
+object isZeroSpecification extends Properties("isZero"):
+
+  property("Zero is zero") = propBoolean:
+    Zero.isZero
+
+  property("Successor is not zero") = forAll: (n: Numeral) =>
+    !Successor(n).isZero
+
+end isZeroSpecification
+
+object PredecessorSpecification extends Properties("Predecessor"):
+
+  property("Zero predecessor is Zero") = propBoolean:
+    Zero.predecessor == Zero
+
+  property("Successor predecessor is the original numeral") = forAll: (n: Numeral) =>
+    Successor(n).predecessor == n
+
+  property("n predecessor is (n - 1) for n != 0") = forAll: (n: Numeral) =>
+    !n.isZero ==> (n.predecessor.toInt == (n.toInt - 1))
+
+end PredecessorSpecification
+
+object SuccessorSpecification extends Properties("Successor"):
+
+  property("Zero successor is Successor(Zero)") = propBoolean:
+    Zero.successor == Successor(Zero)
+
+  property("Successor successor is Successor(Successor)") = forAll: (n: Numeral) =>
+    Successor(n).successor == Successor(Successor(n))
+
+  property("n successor is (n + 1)") = forAll: (n: Numeral) =>
+    n.successor.toInt == (n.toInt + 1)
+
+end SuccessorSpecification
+
+object AdditionSpecification extends Properties("Addition"):
+
+  property("Zero + Zero == Zero") = propBoolean:
+    (Zero + Zero) == Zero
+
+  property("Zero + n == n") = forAll: (n: Numeral) =>
+    (Zero + n) == n
+
+  property("Successor(n) + Zero == Successor(n)") = forAll: (n: Numeral) =>
+    (Successor(n) + Zero) == Successor(n)
+
+  property("Successor(n) + Successor(m) == Successor(Successor(n + m))") = forAll: (n: Numeral, m: Numeral) =>
+    (Successor(n) + Successor(m)) == Successor(Successor(n + m))
+
+  property("Addition is commutative") = forAll: (a: Numeral, b: Numeral) =>
+    (a + b).toInt == (b + a).toInt
+
+  property("Addition is associative") = forAll: (a: Numeral, b: Numeral, c: Numeral) =>
+    ((a + b) + c).toInt == (a + (b + c)).toInt
+
+end AdditionSpecification
+
+object SubtractionSpecification extends Properties("Subtraction"):
+
+  property("Zero - Zero == Zero") = propBoolean:
+    (Zero - Zero) == Zero
+
+  property("Zero - n is Zero") = forAll: (n: Numeral) =>
+    (Zero - n) == Zero
+
+  property("Successor(n) - Zero == Successor(n)") = forAll: (n: Numeral) =>
+    (Successor(n) - Zero) == Successor(n)
+
+  property("Successor(n) - Successor(m) == n - m") = forAll: (n: Numeral, m: Numeral) =>
+    (Successor(n) - Successor(m)) == n - m
+
+  property("Subtracting larger or equal from value is Zero") = forAll: (a: Numeral, b: Numeral) =>
+    (a - (a + b)) == Zero
+
+end SubtractionSpecification
+
+object CombinedOperationsSpecification extends Properties("CombinedOperations") {
+
+  property("Adding and then subtracting the same value should return the original value") = forAll: (a: Numeral, b: Numeral) =>
+    ((a + b) - b) == a
+
+  property("Subtraction and then addition should return the original value if original value >= subtrahend") = forAll: (a: Numeral, b: Numeral) =>
+    (((a + b) - b) + b) == (a + b)
+
+  property("Subtraction and addition are consistent if original value >= subtrahend") = forAll: (a: Numeral, b: Numeral, c: Numeral, d: Numeral) =>
+    (((a + b) - b) + ((c + d) - d)) == ((a + b) + (c + d)) - (b + d)
+}
+
+object ComparisonSpecification extends Properties("Comparison"):
+
+  property("Zero == Zero") = propBoolean:
+    Zero == Zero
+
+  property("Zero != nonZeroValue") = forAll: (nonZeroValue: Numeral) =>
+    !nonZeroValue.isZero ==> (Zero != nonZeroValue)
+
+  property("Zero > Zero") = propBoolean:
+    !(Zero > Zero)
+
+  property("Zero >= Zero") = propBoolean:
+    Zero >= Zero
+
+  property("Zero < Zero") = propBoolean:
+    !(Zero < Zero)
+
+  property("Zero <= Zero") = propBoolean:
+    Zero <= Zero
+
+  property("Zero > nonZeroValue") = forAll: (nonZeroValue: Numeral) =>
+    !nonZeroValue.isZero ==> !(Zero > nonZeroValue)
+
+  property("Zero >= nonZeroValue") = forAll: (nonZeroValue: Numeral) =>
+    !nonZeroValue.isZero ==> !(Zero >= nonZeroValue)
+
+  property("Zero < nonZeroValue") = forAll: (nonZeroValue: Numeral) =>
+    !nonZeroValue.isZero ==> Zero < nonZeroValue
+
+  property("Zero <= nonZeroValue") = forAll: (nonZeroValue: Numeral) =>
+    !nonZeroValue.isZero ==> Zero <= nonZeroValue
+
+  property("nonZeroValue > Zero") = forAll: (nonZeroValue: Numeral) =>
+    !nonZeroValue.isZero ==> nonZeroValue > Zero
+
+  property("successor(n) == successor(n)") = forAll: (n: Numeral) =>
+    Successor(n) == Successor(n)
+
+  property("successor(n) != successor(m) if n != m") = forAll: (n: Numeral, m: Numeral) =>
+    (n != m) ==> (Successor(n) != Successor(m))
+
+  property("successor(n) > Zero") = forAll: (n: Numeral) =>
+    Successor(n) > Zero
+
+  property("successor(n) >= Zero") = forAll: (n: Numeral) =>
+    Successor(n) >= Zero
+
+  property("Zero < successor(n)") = forAll: (n: Numeral) =>
+    Zero < Successor(n)
+
+  property("Zero <= successor(n)") = forAll: (n: Numeral) =>
+    Zero <= Successor(n)
+
+  property("successor(n) > successor(m) if n > m") = forAll: (n: Numeral, m: Numeral) =>
+    (n > m) ==> (Successor(n) > Successor(m))
+
+  property("successor(n) >= successor(m) if n > m") = forAll: (n: Numeral, m: Numeral) =>
+    (n > m) ==> (Successor(n) >= Successor(m))
+
+  property("successor(n) < successor(m) if n < m") = forAll: (n: Numeral, m: Numeral) =>
+    (n < m) ==> (Successor(n) < Successor(m))
+
+  property("successor(n) <= successor(m) if n < m") = forAll: (n: Numeral, m: Numeral) =>
+    (n < m) ==> (Successor(n) <= Successor(m))
+
+end ComparisonSpecification

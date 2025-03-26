ee98277eaff7e2ac06268626800a5f9508eb03d9
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2025-03-26 16:36:43 +0200

    implement abstract order for Numerals and set

--- src/main/scala/kse/unit6/challenge/order.scala
+++ src/main/scala/kse/unit6/challenge/order.scala
@@ -10,5 +10,25 @@ object order:
 
   extension [V: Order as ord](elem: V)
 
+    @targetName("greater than")
     infix def >(that: V): Boolean =
       ord.compare(elem, that) > 0
+
+    @targetName("greater or equal to")
+    infix def >=(that: V): Boolean =
+      ord.compare(elem, that) >= 0
+
+    @targetName("less than")
+    infix def <(that: V): Boolean =
+      ord.compare(elem, that) < 0
+
+    @targetName("less or equal to")
+    infix def <=(that: V): Boolean =
+      ord.compare(elem, that) <= 0
+
+  given Order[Numeral] with
+
+    def compare(left: Numeral, right: Numeral): Int =
+      if left.toInt < right.toInt then -1
+      else if left.toInt > right.toInt then 1
+      else 0

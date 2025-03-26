6a8d7771628fb4b9bf48371729c6033ed2ed7820
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2025-03-27 01:35:37 +0200

    implement Try ADT

--- src/main/scala/kse/unit7/challenge/adt.scala
+++ src/main/scala/kse/unit7/challenge/adt.scala
@@ -4,11 +4,21 @@ object adt:
 
   enum Try[+V]:
 
-    case DummyCase
+    case Success(x: V)         extends Try[V]
+    case Failure(e: Throwable) extends Try[Nothing]
 
-    def flatMap[Q](f: V => Try[Q]): Try[Q] = ???
+    def flatMap[Q](f: V => Try[Q]): Try[Q] =
+      this match
+        case Try.Failure(e) => Try.Failure(e)
+        case Try.Success(v) => f(v)
 
-    def map[Q](f: V => Q): Option[Q] = ???
+    def map[Q](f: V => Q): Try[Q] =
+      this match
+        case Try.Failure(e) => Try.Failure(e)
+        case Try.Success(v) => Try.Success(f(v))
 
   object Try:
-    def apply[V](v: V): Try[V] = ???
+
+    def apply[V](v: V): Try[V] =
+      if v == null then Try.Failure(new NullPointerException("Value cannot be null"))
+      else Try.Success(v)

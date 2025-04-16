b766e75e544627b1d8c8df5f3f0397b77b2a1bd6
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2025-04-16 16:54:34 +0300

    Unit 7 (#8)
    * implement Try ADT
    
    * implement getPostsViews, getPostsViewDesugared, getPostView and getPostViewDesugared
    
    * fix flatMap, map and getPostsViews methods

--- src/main/scala/kse/unit7/challenge/adt.scala
+++ src/main/scala/kse/unit7/challenge/adt.scala
@@ -4,11 +4,25 @@ object adt:
 
   enum Try[+V]:
 
-    case DummyCase
+    case Success(x: V)         extends Try[V]
+    case Failure(e: Throwable) extends Try[Nothing]
 
-    def flatMap[Q](f: V => Try[Q]): Try[Q] = ???
+    def flatMap[Q](f: V => Try[Q]): Try[Q] =
+      this match
+        case Try.Failure(e) => Try.Failure(e)
+        case Try.Success(v) =>
+          try f(v)
+          catch case ex: Throwable => Try.Failure(ex)
 
-    def map[Q](f: V => Q): Option[Q] = ???
+    def map[Q](f: V => Q): Try[Q] =
+      this match
+        case Try.Failure(e) => Try.Failure(e)
+        case Try.Success(v) =>
+          try Try.Success(f(v))
+          catch case ex: Throwable => Try.Failure(ex)
 
   object Try:
-    def apply[V](v: V): Try[V] = ???
+
+    def apply[V](v: V): Try[V] =
+      if v == null then Try.Failure(new NullPointerException("Value cannot be null"))
+      else Try.Success(v)
